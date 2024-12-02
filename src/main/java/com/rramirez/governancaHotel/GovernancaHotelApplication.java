e2b2646a05cb8b74ae2c4ca09cd91426ecfa3faf
Rodrigo Ramirez <rodrigopinheiroramirez@gmail.com>
2024-12-02 15:25:09 -0300

    Aruivos pradrões

---
+++ src/main/java/com/rramirez/governancaHotel/GovernancaHotelApplication.java
@@ -0,0 +1,13 @@
+package com.rramirez.governancaHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovernancaHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovernancaHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rramirez/governancaHotel/GovernancaHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rramirez.governancaHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovernancaHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
